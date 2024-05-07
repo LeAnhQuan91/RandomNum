731798ca54f840f73cd0a9cb6ef8e4470e146279
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-05-07 13:44:58 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int M;
+        Random random = new Random();
+
+        int N = random.Next(0, 9);
+
+        Console.WriteLine("Nhap 1 so M bat ky tu ban phim co gia tri 0 den 9:");
+        M = Convert.ToInt32(Console.ReadLine());
+        if (N==M)
+        {
+            Console.WriteLine("Du doan dung");
+        }
+        else
+        {
+            Console.WriteLine("Du doan sai");
+        
+        }    
+        Console.WriteLine("Vi so ngau nhien la: {0} , va so ban cho la {1}", N, M) ;
+    }
+}
